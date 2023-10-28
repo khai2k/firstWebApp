ac5b96e21205842c289cd6af4d67a977aeb18d54
ngovankhai <ngovankhai2000@gmail.com>
2023-10-28 08:35:59 +0700

    update content

---
+++ test/route.test.js
@@ -0,0 +1,23 @@
+const chai = require("chai");
+const supertest = require("supertest");
+const app = require("../app"); // Assuming your Express app is in the "app.js" file
+
+const expect = chai.expect;
+const request = supertest(app);
+
+describe("GET /", function () {
+  it("responds with HTML and a title", function (done) {
+    request
+      .get("/")
+      .expect(200)
+      .expect("Content-Type", /html/)
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        // You can add more specific assertions here if needed
+        expect(res.text).to.include("<title>Express 2</title>");
+
+        done();
+      });
+  });
+});
